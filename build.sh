883527ef1718fb76006d8aaacfec779b6cdf253b
Stef <git@srilq.email>
2021-08-16 23:01:13 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 npm exec --no tsc
 npm exec --no esbuild -- getSrcset.ts --platform=neutral --format=cjs --outfile=getSrcset.dist.js
-npm exec --no esbuild -- getSrcset.ts --platform=browser --format=cjs --target=es2017 --minify --outfile=getSrcset.dist.min.js
+npm exec --no esbuild -- getSrcset.ts --platform=browser --format=cjs --target=es6 --minify --outfile=getSrcset.dist.min.js
